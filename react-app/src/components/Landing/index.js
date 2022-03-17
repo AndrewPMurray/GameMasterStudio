3cecd2223dd8d5e77d6eb921ce84565c03d86297
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-03-17 12:59:22 -0400

    basic splash page set up

--- react-app/src/components/Landing/index.js
+++ react-app/src/components/Landing/index.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './Landing.css';
 
@@ -12,7 +13,24 @@ const Landing = () => {
 		if (user) history.push('/home');
 	}, [user]);
 
-	return <h1>GameMasterStudio</h1>;
+	return (
+		<div className='landing-container'>
+			<h1 id='header'>GameMasterStudio</h1>
+			<h2>
+				Create characters and campaigns to keep track of what happened in all of your
+				sessions
+			</h2>
+			<p>
+				Every NPC, every item of importance in your bag of holding, every action that
+				chaotic-evil player ever performed. None of it needs to be forgotten. Here you can
+				keep all of that information organized and accessible with the click of a button.
+				There be dragons here. Best to keep track of them.
+			</p>
+			<p>
+				<Link to='/sign-up'>Click here to sign up!</Link>
+			</p>
+		</div>
+	);
 };
 
 export default Landing;
--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import './NavBar.css';
 
 const NavBar = () => {
+	const user = useSelector((state) => state.session.user);
+
 	return (
-		<div style={{ backgroundImage: 'url(/images/navbar-background.jpg)' }}>
+		<div id='navbar-image' style={{ backgroundImage: 'url(/images/navbar-background.jpg)' }}>
 			<nav id='navbar-container'>
 				<div id='nav-items'>
 					<NavLink to='/' exact={true} activeClassName='active'>
 						Home
 					</NavLink>
 					<div id='login-logout-container'>
-						<NavLink to='/login' exact={true} activeClassName='active'>
-							Login
-						</NavLink>
-						<NavLink to='/sign-up' exact={true} activeClassName='active'>
-							Sign Up
-						</NavLink>
-						<LogoutButton />
+						{!user && (
+							<div>
+								<NavLink to='/login' exact={true} activeClassName='active'>
+									Login
+								</NavLink>
+								<NavLink to='/sign-up' exact={true} activeClassName='active'>
+									Sign Up
+								</NavLink>
+							</div>
+						)}
+						{user && <LogoutButton />}
 					</div>
 				</div>
 			</nav>
