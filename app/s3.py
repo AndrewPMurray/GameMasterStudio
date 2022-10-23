3f83d6eab7b0bce053626a66051f2791faaf30d4
Andrew Murray <andrewpmurray1987@gmail.com>
2022-10-23 15:47:51 -0400

    begin migration from aws to minio s3

---
+++ app/s3.py
@@ -0,0 +1,19 @@
+from minio import Minio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BUCKET_NAME = os.environ.get("MINIO_BUCKET")
+ACCESS_KEY = os.environ.get("MINIO_KEY")
+SECRET_KEY = os.environ.get("MINIO_SECRET")
+
+MINIO_API_HOST = "http://192.168.1.112:9000"
+MINIO_CLIENT = Minio("192.168.1.112:9000", access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
+
+def main():
+    found = MINIO_CLIENT.bucket_exists(BUCKET_NAME)
+    if not found:
+        print("Bucket does not exist!")
+    else:
+        print("Located bucket, minio initialized!!")
