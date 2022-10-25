d4df05a1e4f304de0ca3864e6477bb55f18529d6
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-10-25 12:17:56 -0400

    convert static s3 urls to minio

--- app/s3.py
+++ app/s3.py
@@ -2,21 +2,45 @@ from minio import Minio
 from minio.error import S3Error
 from dotenv import load_dotenv
 import os
+import uuid
 
 load_dotenv()
 
 BUCKET_NAME = os.environ.get("MINIO_BUCKET")
 ACCESS_KEY = os.environ.get("MINIO_KEY")
 SECRET_KEY = os.environ.get("MINIO_SECRET")
+is_production = os.environ.get("FLASK_ENV") == 'production'
 
-MINIO_API_HOST = "http://192.168.1.112:9000"
-MINIO_CLIENT = Minio("192.168.1.112:9000", access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
+
+MINIO_API_HOST = "theelderwan.us.to:9000" if is_production else "192.168.1.112:9000"
+s3 = Minio(MINIO_API_HOST, access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
 
 def isOnline():
-    found = MINIO_CLIENT.bucket_exists(BUCKET_NAME)
+    found = s3.bucket_exists(BUCKET_NAME)
     if not found:
         return False
     
     return True
-        
-isOnline()
+
+def allowed_file(filename):
+    return "." in filename and \
+           filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_unique_filename(filename):
+    ext = filename.rsplit(".", 1)[1].lower()
+    unique_filename = uuid.uuid4().hex
+    return f"{unique_filename}.{ext}"
+
+def upload_file_to_s3(file, acl="public-read"):
+    try:
+        s3.fput_object(
+            BUCKET_NAME,
+            file,
+            file.filename,
+        )
+    except Exception as e:
+        # in case our s3 upload fails
+        return {"errors": str(e)}
+
+    return {"url": f"{MINIO_API_HOST}{BUCKET_NAME}/{file.filename}"}
