123048f94251913fcb4cb732e4710f6e55d86b41
Andrew Murray <andrewpmurray1987@gmail.com>
2022-12-24 15:55:59 -0500

    attempt to fix schema setup

--- app/models/campaign.py
+++ app/models/campaign.py
@@ -1,5 +1,4 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
-from .campaign_user import campaign_users
 
 
 class Campaign(db.Model):
@@ -13,7 +12,7 @@ class Campaign(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     game_master_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
     
-    users = db.relationship("User", secondary=campaign_users, back_populates='campaigns')
+    users = db.relationship("User", secondary=add_prefix_for_prod("campaign_users"), back_populates='campaigns')
     characters = db.relationship("Character", back_populates="campaign")
     sections = db.relationship("Section", back_populates="campaign", cascade="all, delete")
     game_master = db.relationship("User", foreign_keys=[game_master_id])
