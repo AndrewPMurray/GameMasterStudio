15afe03862249511a8e76ff09522d4b97453a4dc
AndrewPMurray <AndrewPMurray1987@gmail.com>
2024-05-20 19:50:07 -0400

    attempt to fix 404 errors

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,8 +7,6 @@ import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar/';
 import ProtectedRoute from './components/auth/ProtectedRoute';
-// import UsersList from './components/UsersList';
-// import User from './components/User';
 import UserHome from './components/UserHome/';
 import Landing from './components/Landing';
 import CharacterForm from './components/CharacterForm';
